698f323a9c7cbc41745d20c34b3e2e85080fd31f
Zakaria Jaouad <zzjaouad@gmail.com>
2019-06-04 22:50:55 -0400

    Master: Added implementation of first challenge of the course.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,16 +1,65 @@
 'use strict';
 
-// Auto generated file from babel
-
 console.log('App.js is running');
 
 // JSX - JavaScript XML
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item 1'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item 2'
+        )
+    )
 );
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Zakaria Jaouad'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 26'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: East Providence'
+    )
+);
+
+// Challenge 1:
+// create tempalteTwo
+// h1 with your name
+// p tag with your age
+// location
+// Render templateTwo instead of template
+
 var appRoot = document.getElementById('app');
 
 // takes 2 arguments
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,10 +1,33 @@
-// Auto generated file from babel
-
 console.log('App.js is running');
 
 // JSX - JavaScript XML
-var template = <h1>Indecision App</h1>;
+var template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item 1</li>
+            <li>Item 2</li>
+        </ol>
+    </div>
+);
+
+var templateTwo = (
+    <div>
+        <h1>Zakaria Jaouad</h1>
+        <p>Age: 26</p>
+        <p>Location: East Providence</p>
+    </div>
+);
+
+// Challenge 1:
+    // create tempalteTwo
+    // h1 with your name
+    // p tag with your age
+    // location
+// Render templateTwo instead of template
+
 var appRoot = document.getElementById('app');
 
 // takes 2 arguments
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
